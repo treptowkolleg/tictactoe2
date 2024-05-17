e2896b72d0535fa2cada3aabd31e36ae9ccf90f0
Treptow Kolleg <it.treptowkolleg.de>
2024-05-17 20:16:23 +0200

    add php-gd icons

--- templates/gameboard.html.php
+++ templates/gameboard.html.php
@@ -6,8 +6,10 @@ $player = $_SESSION['player'];
 
 <h1>Gamebaord</h1>
 
-<h2>Aktuell an der Reihe: <?=$player?></h2>
+<h2>Aktuell an der Reihe: <img src="/icon.php?icon=<?=strtolower($player)?>" alt="aktueller Spieler"></h2>
 
+<h3>Zufallszahl</h3>
+<p><?=rand(1,1000)?></p>
 <table>
     <?php for($y = 1; $y <= 3; $y++): ?>
         <tr> <!-- Zeile /-->
@@ -19,7 +21,7 @@ $player = $_SESSION['player'];
 
                         foreach($_SESSION['played_fields'] as $field) {
                             if($x == $field['x'] and $y == $field['y']) {
-                                echo $field['player'];
+                                echo '<img src="/icon.php?icon=' . strtolower($field['player']) . '" alt="'.$field['player'].'">';
                                 $found = true;
                                 break;
                             }
@@ -27,7 +29,7 @@ $player = $_SESSION['player'];
 
                         if(!$found) {
                             //echo sprintf('<a href="/?x=%s&y=%s">Spielen</a>',$x,$y);    // Das gleiche
-                            echo '<a href="/?x='.$x.'&y='.$y.'">Spielen</a>';                 // wie das hier
+                            echo '<a href="/?x='.$x.'&y='.$y.'"><img src="/icon.php" alt="Spielen"></a>';                 // wie das hier
                             //echo "<a href=\"/?x=$x&y=$y\">Spielen</a>";                 // wie das hier
                         }
 
